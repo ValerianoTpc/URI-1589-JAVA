92fb902cd37d79f38036d750166b08dfd5a5770b
Lucas Valeriano <47339803+ValerianoTpc@users.noreply.github.com>
2019-02-09 00:11:49 -0200

    Add files via upload

---
+++ 1589.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ Scanner ok = new Scanner (System.in);
+
+			
+		int n,a,b;
+			n= ok.nextInt();
+			
+			for(int i=0;i<n;i++) {
+				
+				a = ok.nextInt();
+				b=ok.nextInt();
+				System.out.println(a+b);
+				
+				
+			}
+			
+		}
+	}
